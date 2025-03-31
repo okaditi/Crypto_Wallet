66596e3f6612ed02a4762295c38585ad46dbbfd8
Aditi Singh <aditisingh01299@gmail.com>
2025-04-01 02:24:14 +0530

    updating code

--- lib/services/wallet_services.dart
+++ lib/services/wallet_services.dart
@@ -10,7 +10,7 @@ import 'package:walletconnect_dart/walletconnect_dart.dart';
 //setting up dependencies/tools we will use throughout the service!!!
 class WalletService {
   final storage = FlutterSecureStorage(); //saves private key on the devices
-  final String rpcUrl = "https://sepolia.infura.io/v3/YOUR_INFURA_KEY"; // Infura API- intracting w blockchain using etherum node url which in this case is infura, an ethereum provider and i fucking forgot mine so i have to ask bhaiya
+  final String rpcUrl = "https://sepolia.infura.io/v3/235b57e865d249359ec1aebd2c620c39"; // Infura API- intracting w blockchain using etherum node url which in this case is infura, an ethereum provider and i fucking forgot mine so i have to ask bhaiya
   final HushWalletService hushWalletService = HushWalletService();
   late Web3Client ethClient; // lets us send transaction,get balance, interact with smart contracts
   late WalletConnect connector; //connects the metamask wallet to our app
